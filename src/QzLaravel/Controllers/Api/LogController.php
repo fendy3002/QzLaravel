abc097e0eebb281a621014604bf1d741af1988e7
Fendy Heryanto <fendy.heryanto@coderscolony.com>
2016-11-11 17:05:03 +0700

    Added prototype for getDetail

--- src/QzLaravel/Controllers/Api/LogController.php
+++ src/QzLaravel/Controllers/Api/LogController.php
@@ -5,12 +5,13 @@ namespace QzLaravel\Controllers\Api;
 use App\User;
 use App\Http\Controllers\Controller;
 use Request;
+use QzPhp\Q;
 
 class LogController extends Controller
 {
     public function getList()
     {
-        $path = storage_path('log');
+        $path = storage_path('logs');
         $files = array_diff(scandir($path), array('.', '..'));
 
         $response = [
@@ -19,11 +20,18 @@ class LogController extends Controller
         return response(json_encode($response), 200);
     }
 
-    public function getDetail($file)
+    public function getDetail($file, $fromEnd = 1, $lastPos = 0, $limit = 100)
     {
-        $response = [
+        $fileReader = new \QzPhp\FileReader();
+        $filePath = Q::Z()->io()->combine(storage_path('logs'), $file);
+        if($fromEnd == 1){
+            $read = $fileReader->readFileByLinesR($filePath, $lastPos, $limit);
+        }
+        else{
+            $read = $fileReader->readFileByLines($filePath, $lastPos, $limit);
+        }
 
-        ];
+        $response = $read;
         return response(json_encode($response), 200);
     }
 
